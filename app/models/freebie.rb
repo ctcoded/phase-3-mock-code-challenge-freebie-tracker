e802cb6900b44792c97afb0df7f7f7bb630dc6d5
cesartidrick <cesartidrick@gmail.com>
2022-08-22 16:38:18 -0600

    commit tree 6e270a3b388d2c0518318c259a68ed3f8d367801

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,12 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :dev
+    belongs_to :company
+
+    def print_details 
+        "#{dev.name} owns a #{item_name} from #{company.name}"
+    end
 
 end
+
+
+# need to ask matt why I can't get any info in rake console
